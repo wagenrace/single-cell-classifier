9162d27046a0e229c6e7f1fe2fcb75bf88db38a5
wagenrace <nijhof.dns@gmail.com>
2019-12-06 15:38:13 +0100

    add support vector machines plus one run

---
+++ 3.train_ebimage/2.support_vector_machine/svm_eigen_values.py
@@ -0,0 +1,39 @@
+# %% Initialization
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+
+basefolder_loc = Path(__file__).parents[2]
+sys.path.append(str(basefolder_loc))
+
+from utils import create_metrics
+
+data_dir = os.path.join(basefolder_loc, "2.process-data", "data")
+
+
+def load_data(file_name="train_eigen_values.tsv.gz"):
+    file_loc = os.path.join(data_dir, file_name)
+    data = pd.read_csv(file_loc, sep="\t")
+    X = np.array(data.drop(columns=["cell_codes", "targets"]))
+    Y = np.array(data.targets)
+    return X, Y
+
+
+# %% Training model
+clf = SVC()
+X, Y = load_data("train_eigen_values.tsv.gz")
+clf.fit(X, Y)
+
+# %% Validation of model
+valX, valY = load_data("test_eigen_values.tsv.gz")
+prediction = clf.predict(valX)
+
+create_metrics(
+    prediction,
+    valY,
+    os.path.join(os.path.join(os.path.dirname(__file__)), "results_eigenvalue"),
+)
